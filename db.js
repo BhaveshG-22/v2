46f0048162f66e798d4a594aa357a52bf357c74a
BhaveshG-22 <107833962+BhaveshG-22@users.noreply.github.com>
2023-03-13 00:05:46 -0400

    Add files via upload

---
+++ db.js
@@ -0,0 +1,32 @@
+const { MongoClient } = require('mongodb')
+const env = require('dotenv').config()
+
+const express = require('express')
+const app = express()
+app.use(express.json())
+// env
+
+// Connection URL
+const url = 'mongodb://localhost:27017';
+const client = new MongoClient(url);
+
+let dbConnection
+let uri = process.env.MONGODB_URI
+
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient.connect(uri)
+            .then((client) => {
+                dbConnection = client.db()
+
+                return cb()
+            })
+            .catch(err => {
+                console.log(err)
+                return cb(err)
+            })
+    },
+    getDb: () => dbConnection
+
+}
